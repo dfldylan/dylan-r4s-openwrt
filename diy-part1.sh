edb4e8a1fcc1dc629e6e827bbcf8173a72f1f680
dylan <dfldylan@qq.com>
2022-04-28 22:07:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
+git clone -b legacy https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
